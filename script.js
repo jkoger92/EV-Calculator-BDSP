993179461aefc185f8e05fcad2977fa2908231d9
jkoger92 <jkoger92@yahoo.com>
2021-12-11 14:37:41 -0600

    Added in atk, def, spd, spe variables to calculator

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
+//counter variable
 const counter = document.getElementById('counter')
+
+//Special Attack variables
 const roseliaBtn = document.getElementById('roselia')
 const budewBtn = document.getElementById('budew')
+const golduckBtn = document.getElementById('golduck')
+
+//HP variables
+const gastrodonBtn = document.getElementById('gastrodon')
+
+//Attack variables
+const bibarelBtn = document.getElementById('bibarel')
+const kricketuneBtn = document.getElementById('kricketune')
+
+//Defense variable
+const silcoonBtn = document.getElementById('silcoon')
+const cascoonBtn = document.getElementById('cascoon')
+const gravelerBtn = document.getElementById('graveler')
+
+//Special defense variables
+const girafarigBtn = document.getElementById('girafarig')
+const tentacruelBtn = document.getElementById('tentacruel')
+
+//Speed variables
+const floatzelBtn = document.getElementById('floatzel')
+
+//Reset variable
 const resetBtn = document.getElementById('reset')
+
+//Pokerus variables
 const pokerusOnBtn = document.getElementById('pokerusOn')
 const pokerusOffBtn = document.getElementById('pokerusOff')
 
 let counterValue = 0
 let pokerusActive = false
 
+//Pokerus button functions
 pokerusOffBtn.addEventListener('click', () => {
   pokerusActive = false
   pokerusOnBtn.style.background = ''
@@ -16,6 +44,8 @@ pokerusOnBtn.addEventListener('click', () => {
   pokerusActive = true
   pokerusOnBtn.style.backgroundColor = '#FF0000'
 })
+
+//Special attack button functions
 roseliaBtn.addEventListener('click', () => {
   if (pokerusActive == false) {
     counterValue += 2
@@ -36,6 +66,118 @@ budewBtn.addEventListener('click', () => {
     counter.innerHTML = counterValue
   }
 })
+golduckBtn.addEventListener('click', () => {
+  if (pokerusActive == false) {
+    counterValue += 2
+    counter.innerHTML = counterValue
+  }
+  if (pokerusActive == true) {
+    counterValue += 4
+    counter.innerHTML = counterValue
+  }
+})
+
+//HP button functions
+gastrodonBtn.addEventListener('click', () => {
+  if (pokerusActive == false) {
+    counterValue += 2
+    counter.innerHTML = counterValue
+  }
+  if (pokerusActive == true) {
+    counterValue += 4
+    counter.innerHTML = counterValue
+  }
+})
+
+//Attack button functions
+bibarelBtn.addEventListener('click', () => {
+  if (pokerusActive == false) {
+    counterValue += 2
+    counter.innerHTML = counterValue
+  }
+  if (pokerusActive == true) {
+    counterValue += 4
+    counter.innerHTML = counterValue
+  }
+})
+kricketuneBtn.addEventListener('click', () => {
+  if (pokerusActive == false) {
+    counterValue += 2
+    counter.innerHTML = counterValue
+  }
+  if (pokerusActive == true) {
+    counterValue += 4
+    counter.innerHTML = counterValue
+  }
+})
+
+//Defense button functions
+silcoonBtn.addEventListener('click', () => {
+  if (pokerusActive == false) {
+    counterValue += 2
+    counter.innerHTML = counterValue
+  }
+  if (pokerusActive == true) {
+    counterValue += 4
+    counter.innerHTML = counterValue
+  }
+})
+cascoonBtn.addEventListener('click', () => {
+  if (pokerusActive == false) {
+    counterValue += 2
+    counter.innerHTML = counterValue
+  }
+  if (pokerusActive == true) {
+    counterValue += 4
+    counter.innerHTML = counterValue
+  }
+})
+gravelerBtn.addEventListener('click', () => {
+  if (pokerusActive == false) {
+    counterValue += 2
+    counter.innerHTML = counterValue
+  }
+  if (pokerusActive == true) {
+    counterValue += 4
+    counter.innerHTML = counterValue
+  }
+})
+
+//Special defense button functions
+girafarigBtn.addEventListener('click', () => {
+  if (pokerusActive == false) {
+    counterValue += 2
+    counter.innerHTML = counterValue
+  }
+  if (pokerusActive == true) {
+    counterValue += 4
+    counter.innerHTML = counterValue
+  }
+})
+tentacruelBtn.addEventListener('click', () => {
+  if (pokerusActive == false) {
+    counterValue += 2
+    counter.innerHTML = counterValue
+  }
+  if (pokerusActive == true) {
+    counterValue += 4
+    counter.innerHTML = counterValue
+  }
+})
+
+//Speed button functions
+floatzelBtn.addEventListener('click', () => {
+  if (pokerusActive == false) {
+    counterValue += 2
+    counter.innerHTML = counterValue
+  }
+  if (pokerusActive == true) {
+    counterValue += 4
+    counter.innerHTML = counterValue
+  }
+})
+
+//Reset button functions
 resetBtn.addEventListener('click', () => {
   counterValue = 0
   counter.innerHTML = counterValue
