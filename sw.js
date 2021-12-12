8aa7ef5c715d853e1705bb9eef542237a1a9c62e
jkoger92 <jkoger92@yahoo.com>
2021-12-11 19:04:05 -0600

    Make website a PWA

---
+++ sw.js
@@ -0,0 +1,26 @@
+// On install - the application shell cached
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('sw-cache').then(function (cache) {
+      //Static files that make up the application shell are cached
+      return cache.add(
+        'index.html',
+        'hp.html',
+        'attack.html',
+        'defense.html',
+        'specialDefense.html',
+        'speed.html',
+      )
+    }),
+  )
+})
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    //Try the cache
+    caches.match(event.request).then(function (response) {
+      //return it if there is a response, or else fetch again
+      return response || fetch(event.request)
+    }),
+  )
+})
